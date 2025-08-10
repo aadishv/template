888030a994f3d406bf4bbe0218ce844f5f57e178
StarLight842 <842starlite@gmail.com>
2025-08-10 11:13:45 -0700

    chore: gut convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,28 +7,5 @@ import { authTables } from "@convex-dev/auth/server";
 // The schema provides more precise TypeScript types.
 export default defineSchema({
   ...authTables,
-  saved: defineTable({
-    user: v.id("users"),
-    song: v.number(),
-  }).index("user", ["user"]),
-  comments: defineTable({
-    user: v.id("users"),
-    song: v.number(),
-    start: v.number(),
-    end: v.number(),
-    color: v.string(),
-    title: v.string(),
-    content: v.string(),
-  })
-    .index("song_and_user", ["song", "user"])
-    .index("user", ["user"]),
-  linkedComments: defineTable({
-    comment: v.id("comments"),
-    start: v.number(),
-    end: v.number(),
-    song: v.number(),
-    user: v.id("users"),
-  })
-    .index("song_and_user", ["song", "user"])
-    .index("user", ["user"]),
+  // add your own tables here!
 });
